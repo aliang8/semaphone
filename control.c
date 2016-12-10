52ff7145298f5a4a009185194718f35600035c1d
Anthony Liang <aliang8@stuy.edu>
2016-12-10 17:06:51 -0500

    finished control.c

--- control.c
+++ control.c
@@ -34,7 +34,7 @@ int main(int argc, char *argv[]) {
   int semid;
   int key = ftok("makefile" , 22);
   int sc; 
-  struct shmid_ds d;
+  //struct shmid_ds d;
   
   if(argv[1] == NULL){
     printf("Please provide an argument!\n");
@@ -42,39 +42,41 @@ int main(int argc, char *argv[]) {
   }
   
   if (strncmp(argv[1], "-c", strlen(argv[1])) == 0){
-    //create shared mem segment    
     int shm_id;
     char* data;
     int fd;
     int *shm_ptr;
-    
-    shm_id = shmget( ftok("story.txt",22), 1024, IPC_CREAT | 0644 );
-    if (shm_id < 0){
-      printf("shmget error\n");
-      exit(1);
-    }
-    
-    //attaching the shared memory segment
-    shm_ptr = (int *)shmat(shm_id, NULL, 0);
-    if ((int) shm_ptr == -1){
-      printf("shmat error\n");
-      exit(1);
-    }
-    
-    //created a semaphore
+    int value;
+
     semid = semget(key, 1, IPC_CREAT | IPC_EXCL | 0644);
     if (semid) {
       //create the text file
-      fd = open("story.txt", O_CREAT | 0644);
+      fd = open("story.txt", O_CREAT | O_TRUNC | 0644);
       close(fd);
-    }
-    
-        
-    union semun su;
-    su.val = 1;
-    //setting semaphore value
-    sc = semctl(semid, 0, SETVAL, su);
-    printf("value set: %d\n", sc);
+
+      value = 1;
+      if (argv[2] != NULL){
+	value = atoi(argv[2]);
+      }
+
+      //creating and attaching shared memory segment
+      shm_id = shmget(key, 4, IPC_CREAT | 0644);
+      shm_ptr = (int *) shmat(shm_id,0,0);
+
+      //detaching from shared memory pointer
+      shm_ptr = 0;
+      shmdt(shm_ptr);
+
+      union semun su;
+      su.val = value;
+      //setting semaphore value
+      sc = semctl(semid, 0, SETVAL, su);
+      printf("value set: %d\n", sc);
+      
+    } else {
+      printf("Semaphore already exists!\n");
+      return 1;
+    } 
     
   } else if (strncmp(argv[1], "-v", strlen(argv[1])) == 0){
     //opening the file and reading/outputing recursively
@@ -87,6 +89,7 @@ int main(int argc, char *argv[]) {
       fclose(fp);
     }
     return 0;
+    
   } else if(strncmp(argv[1], "-r", strlen(argv[1])) == 0){
     //removing a semaphore and shared memory segment
     int sm = shmget(key,4,0644);
@@ -95,7 +98,7 @@ int main(int argc, char *argv[]) {
     //removing semaphore
     sc = semctl(semid, 0, IPC_RMID);
     //removing sm segment
-    int smc = shmctl(semid, IPC_RMID, &sm);
+    int smc = shmctl(semid, IPC_RMID, 0); //&sm
     printf("Story removed: %d\n", sc);
     
     //print out the story one more time
